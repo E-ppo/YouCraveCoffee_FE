e28faba45dbc58b3d8666e84ef922c239843201b
epppo <amorosso@kakao.com>
2022-08-10 23:29:37 +0900

    project init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import React from 'react';
 import store from './redux/configStore';
@@ -24,8 +23,3 @@ root.render(
       </Provider>
    </CookiesProvider> 
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
