fecb58f0af98b700032a535b7640df536b5e73cd
epppo <amorosso@kakao.com>
2022-08-20 23:28:04 +0900

    #12 소셜 로그인 SVG 추가 /

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -1,5 +1,8 @@
 import { ReactComponent as LeftArrow } from './leftArrow.svg';
 import { ReactComponent as RightArrow } from './rightArrow.svg';
 import { ReactComponent as SearchIcon } from './searchIcon.svg';
+import { ReactComponent as Google } from './google.svg';
+import { ReactComponent as Kakao } from './kakao.svg';
+import { ReactComponent as Naver } from './naver.svg';
 
-export { LeftArrow, RightArrow, SearchIcon } 
+export { LeftArrow, RightArrow, SearchIcon, Google, Kakao, Naver } 
